cd34242d1a84637a8116a86b9cc21991498e853d
Ujjwol Kayastha <uzol123@gmail.com>
2021-05-03 18:56:21 +0545

    feat: add redux, material ui, firebase

---
+++ src/firebase.js
@@ -0,0 +1,25 @@
+import firebase from "firebase/app";
+
+import "firebase/firestore";
+import "firebase/auth";
+import "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+const analytics = firebase.analytics;
+
+export { firestore, firebase, auth, analytics };
